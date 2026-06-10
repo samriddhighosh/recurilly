660d4a6ab9b3ff1ce17a99399fba9544dd253acf
SomisCreations <89362410+samriddhighosh@users.noreply.github.com>
2026-06-09 20:37:25 -0500

    authentication

--- constants/data.ts
+++ constants/data.ts
@@ -4,11 +4,11 @@ export const tabs: AppTab[] = [
   { name: "index", title: "Home", icon: icons.home },
   { name: "subscriptions", title: "Subscriptions", icon: icons.wallet },
   { name: "insights", title: "Insights", icon: icons.activity },
-  { name: "settings", title: "Settings", icon: icons.setting },
+  { name: "setting", title: "Settings", icon: icons.setting },
 ];
 
 export const HOME_USER = {
-  name: "Adrian | JS Mastery",
+  name: "Samriddhi Ghosh",
 };
 
 export const HOME_BALANCE = {
